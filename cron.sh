8e6037b0d2bf2f8f57c6347dba085964aaf668e0
Divyanshu Bagga <divyanshu.bagga@pm.me>
2021-03-07 16:41:02 +0530

    Added background icon

--- cron.sh
+++ cron.sh
@@ -4,6 +4,6 @@ dir=$(cd `dirname $0` && pwd)
 cd $dir
 
 python3 -m  server.update
-google-chrome --headless --screenshot="screenshot.png" 'http://divyanshubagga.s3-website.us-east-2.amazonaws.com/display.html' --window-size=600,800
+google-chrome --headless --screenshot="screenshot.png" 'display.html' --window-size=600,800
 pngcrush -c 0 screenshot.png display.png >> /dev/null
 aws s3 cp ./display.png s3://divyanshubagga/
